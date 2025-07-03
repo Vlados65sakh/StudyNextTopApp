2ce300fd33ac63fbb74cf24bb51ff2be1f628610
VladislavChurbakov <v.churbakov@voskhod.ru>
2025-07-03 11:13:46 +0800

    Все сделано для урока 10.8

--- top-app/components/Main/Footer/Footer.tsx
+++ top-app/components/Main/Footer/Footer.tsx
@@ -1,11 +1,21 @@
 import {FooterProps} from "./Footer.props";
 import {JSX} from "react";
-// import styles from './Footer.module.css';
+import styles from './Footer.module.css';
+import cn from "classnames";
+import Link from "next/link";
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>
+                OwlTop &copy; 2020 - {new Date().getFullYear()} Все права защищены
+            </div>
+            <Link href={`https://www.youtube.com/watch?v=l_7e2ZamUpI`}>
+                Пользовательское соглашение
+            </Link>
+            <Link href={`https://vkvideo.ru/video-171965386_456239017?ref_domain=yastatic.net`}>
+                Политика конфиденциальности
+            </Link>
+        </footer>
     );
 };
